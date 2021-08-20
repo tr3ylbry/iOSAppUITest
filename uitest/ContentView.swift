0b7743cfd24852d518d9f074e93d62ec01184047
Trey Bryant <harrylbryant@email.arizona.edu>
2021-08-19 22:35:10 -0700

    First commit of code

---
+++ uitest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  uitest
+//
+//  Created by Harry L. Bryant III on 6/27/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, cruel world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ uitest/uitestApp.swift
@@ -0,0 +1,17 @@
+//
+//  uitestApp.swift
+//  uitest
+//
+//  Created by Harry L. Bryant III on 6/27/21.
+//
+
+import SwiftUI
+
+@main
+struct uitestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
